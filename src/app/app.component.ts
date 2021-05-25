017312ca9bfe1aa271339979f6f57760236fc9e0
Rodolfo Christian Catunta Uturunco <rodolfo.catunta.uturunco@gmail.com>
2021-05-25 12:36:38 -0400

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { assetUrl } from 'src/single-spa/asset-url';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-one';
+  imageUrl = assetUrl('../assets/escudo.png');
+  title = 'angular1';
+  name = '';
+
+  sendName(){
+    console.log("Send name",this.name);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { APP_BASE_HREF } from '@angular/common';
+import {FormsModule} from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    {provide: APP_BASE_HREF, useValue: '/'} // Add providers Manually
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
